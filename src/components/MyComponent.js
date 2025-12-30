08505ed2c27e1b1b94b750e623cb1a7325bb1140
dangnhuquynh <dangnhuquynh2403@gmail.com>
2025-12-30 22:01:42 +0700

    Bài 7

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -3,11 +3,19 @@
 
 import React from "react";
 import UserInfo from "./UserInfo";
+import DisplayInfo from "./DisplayInfo";
 class MyComponent extends React.Component {
     render() {
+        // const myAge = 50;
+        const myInfo = ['a', 'b', 'c'];
         return (
             <div>
                 <UserInfo></UserInfo>
+                <br />
+                <br />
+                <DisplayInfo name="Như Quỳnh" age="22"></DisplayInfo>
+                <hr />
+                <DisplayInfo name="Nhu Quynh" age={26} myInfo={myInfo}></DisplayInfo>
             </div>
         )
     }
